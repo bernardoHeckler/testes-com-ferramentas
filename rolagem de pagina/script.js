9b77012c32e99a2b1e00ab512982aabf1bb69173
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2024-08-24 23:33:54 -0300

    teste com rolagem de pagina

---
+++ rolagem de pagina/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('resize', () => {
+    document.body.style.minHeight = `${window.innerHeight}px`;
+});
+
+// Inicializa a altura no carregamento da página
+document.body.style.minHeight = `${window.innerHeight}px`;
